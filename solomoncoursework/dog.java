14daadc89332bd7147d2597f990a46416ffc30c4
SOLOMONOSCAR <117358966+SOLOMONOSCAR@users.noreply.github.com>
2022-11-03 16:55:11 +0300

    Add files via upload

---
+++ solomoncoursework/dog.java
@@ -0,0 +1,24 @@
+package solomoncoursework;
+//MUHANGUZI SOLOMON OSCAR//
+//2021/A/KIT/2604/F//
+public class dog {
+	public static void main(String[]args) {
+		int x,y,z,sum,product;
+		x=8;
+		y=6;
+		z=9;
+		sum=x+y+z;
+		product=x*y*z;
+		System.out.println(sum);
+		System.out.println(product);
+		//lets learn about reversing numbers//
+				int number = 987654, reverse = 0;  
+				while(number != 0)   
+				{  
+				int remainder = number % 10;  
+				reverse = reverse * 10 + remainder;  
+				number = number/10;  
+				}  
+				System.out.println("The reverse of the given number is: " + reverse);  
+	}
+}
